3fee54c283f2f507eaf43f2219511a1b907b0bbb
HareSRV <rvharesh00@gmail.com>
2019-11-17 10:31:51 +0530

    It's Clobbering Time

--- src/App.js
+++ src/App.js
@@ -35,19 +35,21 @@ constructor() {
               return  (course.current=="True")? 
                   (<div className="bg-white b--dashed green dib br3 pa3 ma2 fa fa-fw fa-book" style={{width:'280px',height:'180px'}}>
                     <div>
-                      <h5>Year: {course.year}</h5>
-                      <p>Semester: {course.semester}</p>
-                      <h3>Course: {course.name}</h3>
+                      <h4>Year: {course.year}</h4>
+                      <h4>Semester: {course.semester}</h4>
+                      <h3 className="">{course.name}</h3>
                     </div>          
                     <input className="bg-black white pointer" type='button' onClick={()=>{alert("OOOOOO")}} value='View Course' style={{marginLeft:'10px'}}  />
-                  </div>)
+                  </div>
+
+                  )
                   :
                   
                   (<div className="bg-white b--dashed red dib br3 pa3 ma2 fa fa-fw fa-book" style={{width:'280px',height:'180px'}}>
                     <div>
-                      <h5>Year: {course.year}</h5>
-                      <p>Semester: {course.semester}</p>
-                      <h3>Course: {course.name}</h3>
+                      <h4>Year: {course.year}</h4>
+                      <h4>Semester: {course.semester}</h4>
+                      <h3>{course.name}</h3>
                     </div>          
                     <input className="bg-black white pointer" type='button' value='View Course' style={{marginLeft:'10px'}}  />
                   </div>  )
@@ -55,13 +57,13 @@ constructor() {
                   
   })
   } 
-// onClick={this.handleClick}
+
   render(){
   return (
     <div className="App outer-container">
       
-       <Menu noOverlay  isOpen={ true } customBurgerIcon={ <img src={logo}/>} pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" } >
-        
+       <Menu noOverlay isOpen={ true } customBurgerIcon={ <img src={logo}/>} pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" } >
+        <p className="red ma2 pa3" href="#" style={{fontSize:"2em"}}><i className="bg-gold red fa-lg fa fa-fw fa-user"></i><span>Faculty</span></p>
         <a href="#" ><i className="fa fa-fw fa-star-o"></i><span>Favorites</span></a>
         <a href="#" ><i className="fa fa-fw fa-bell-o"></i><span>Notification</span></a>
         <a href="#" ><i className="fa fa-fw fa-envelope-o"></i><span>Contact</span></a>
@@ -74,7 +76,11 @@ constructor() {
       <main id="page-wrap" style={{marginLeft:"300px"}}>
 
            <h1>Enrolled Courses </h1>
+           <div className="bg-black ma2 pa3 gold topcorner">
+           <span className="bg-red fa fa-fw fa-star-o"></span>- Previous Semesters<br/>
+           <span className="bg-green fa fa-fw fa-star-o"></span>- Current* Semester
            
+           </div>
            <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
               <div >
                   {this.renderCourses()}
